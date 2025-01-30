08661186aa0e8a8af90729118420c3dd885094a8
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-01-30 16:48:16 +0000

    fix: set config id to not be autoinc

--- entity/src/config.rs
+++ entity/src/config.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, JsonSchema, Serialize, Deserialize)]
 #[sea_orm(table_name = "config")]
 pub struct Model {
-    #[sea_orm(primary_key)]
+    #[sea_orm(primary_key, auto_increment = false)]
     pub id: uuid::Uuid,
 
     pub title: String,
